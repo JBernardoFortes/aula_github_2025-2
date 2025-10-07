e0cf9ecd78b74fe6f9842e2c00118b57625274b6
JBernardoFortes <joseberk2@gmail.com>
2025-10-07 20:43:10 -0300

    chore: finalizar scans e remover imports inuteis

--- Menu.java
+++ Menu.java
@@ -1,49 +1,48 @@
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-	private String title;
-	private List<String> options;
+  private String title;
+  private List<String> options;
 
-	public Menu(List<String> options) {
-		this.title = "Menu";
-		this.options = options;
-	}
+  public Menu(List<String> options) {
+    this.title = "Menu";
+    this.options = options;
+  }
 
-	public Menu(String title, List<String> options) {
-		this.title = title;
-		this.options = options;
-	}
+  public Menu(String title, List<String> options) {
+    this.title = title;
+    this.options = options;
+  }
 
-	public int getSelection() {
-		int op = -1;
-		while (op==-1){
-			System.out.println(title+"\n");
-			int i=0;
-			for (String option : options) {
-				System.out.println(i++ + " - " + option);
-			}
+  public int getSelection() {
+    int op = -1;
+    while (op == -1) {
+      System.out.println(title + "\n");
+      int i = 0;
+      for (String option : options) {
+        System.out.println(i++ + " - " + option);
+      }
 
-			System.out.println("Informe a opcao desejada. ");
-			Scanner s = new Scanner(System.in);
-			String str = s.nextLine();
-			try {
-				op = Integer.parseInt(str);
-			}
-			catch (NumberFormatException e) {
-				op =0;
-			}
-			if (op == 0){
-				return op;
-			}
-			if (op>=i){
-				System.out.println("Opcao errada!");
-				op=-1 ;
-			}
+      System.out.println("Informe a opcao desejada. ");
+      Scanner s = new Scanner(System.in);
+      String str = s.nextLine();
+      try {
+        op = Integer.parseInt(str);
+      } catch (NumberFormatException e) {
+        op = 0;
+      }
+      if (op == 0) {
+        s.close();
+        return op;
+      }
+      if (op >= i) {
+        System.out.println("Opcao errada!");
+        op = -1;
+      }
+      s.close();
+    }
 
-		}
-		return op;
-	}
+    return op;
+  }
 }
